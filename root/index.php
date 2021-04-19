61c5fbba4a6dad622d1eaee989c0f86542e51553
s24hao <70073970+s24hao@users.noreply.github.com>
2021-04-19 11:46:53 -0400

    using bootstrap to edit format of the code

--- root/index.php
+++ root/index.php
@@ -1,5 +1,14 @@
 <html>
 <head>
+<!-- CDN for bootstrap -->
+ <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> 
 </head>
 
 <body>
@@ -12,26 +21,65 @@ echo "<br>";
 echo "<br>";
 ?>
 
-<form action="search_key_word.php">
-  <label for="keyword">Enter A Key Word</label><br>
-  <input type="text" id="keyword" name="keyword" value="your key word"><br>
-  <input type="submit" value="Submit">
-</form> 
+<form class="form-horizontal" action="search_key_word.php">
+<fieldset>
+
+<!-- Form Name -->
+<legend>Search for a joke</legend>
+
+<!-- Search input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="keyword">Search Keyword</label>
+  <div class="col-md-5">
+    <input id="keyword" name="keyword" type="search" placeholder="e.g. a word or a phrase" class="form-control input-md">
+    <p class="help-block">Enter a word to search for jokes in the database</p>
+  </div>
+</div>
+
+<!-- Button -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="submit"></label>
+  <div class="col-md-4">
+    <button id="submit" name="submit" class="btn btn-primary">Button</button>
+  </div>
+</div>
+
+</fieldset>
+</form>
 
 <br>
 <hr>
 <br>
 
-<form action="add_joke.php">
-  <label for="newjoke">Add A New Joke</label><br>
-  <input type="text" id="newjoke" name="newjoke" value="default joke"><br>
-  
-  <label for="newanswer">Add Answer To Joke</label><br>
-  <input type="text" id="newanswer" name="newanswer" value="default answer"><br>
-  
-  <input type="submit" value="Submit">
-</form> 
-
+<form class="form-horizontal" action="add_joke.php">
+<fieldset>
+<!-- Form Name -->
+<legend>Add a joke</legend>
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="newjoke">Enter the text of the new joke</label>  
+  <div class="col-md-5">
+  <input id="newjoke" name="newjoke" type="text" placeholder="question?" class="form-control input-md">
+  <span class="help-block">Enter the joke question</span>  
+  </div>
+</div>
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="newanswer">The answer of the new joke</label>  
+  <div class="col-md-5">
+  <input id="newanswer" name="newanswer" type="text" placeholder="answer!" class="form-control input-md">
+  <span class="help-block">Enter the punchline of your joke</span>  
+  </div>
+</div>
+<!-- Button -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="submit"></label>
+  <div class="col-md-4">
+    <button id="submit" name="submit" class="btn btn-primary">Add a new joke</button>
+  </div>
+</div>
+</fieldset>
+</form>
 
 <?php
 // include "search_key_word.php";
