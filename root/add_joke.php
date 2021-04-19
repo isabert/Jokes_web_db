7fe72f6240f218327f272ca565b9ad519a0f3379
s24hao <70073970+s24hao@users.noreply.github.com>
2021-04-19 00:56:01 -0400

    process special cases such as quotation

--- root/add_joke.php
+++ root/add_joke.php
@@ -4,9 +4,12 @@ include "db_connect.php";
 $new_joke_question =  $_GET["newjoke"];
 $new_joke_answer = $_GET["newanswer"];
 
+$new_joke_question = addslashes($new_joke_question);
+$new_joke_answer = addslashes($new_joke_answer);
+
 echo "<h2>Trying to add new joke $new_joke_question with $new_joke_answer as answer<br></h2>";
 $sql = "INSERT INTO jokes_tables( JokeID, Joke_question, Joke_answer) VALUES (Null,'$new_joke_question','$new_joke_answer')";
-$result = $mysqli->query($sql);
+$result = $mysqli->query($sql) or die(mysqli_error($mysqli));
 
 include "search_all_jokes.php";
 ?>
